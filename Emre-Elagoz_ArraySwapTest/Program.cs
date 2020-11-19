f4eea984daed1815594d17acedc14c3bd256c403
Emre Elagoz <emreelagoz1994@gmail.com>
2020-11-19 09:46:42 +0100

    Add project files.

---
+++ Emre-Elagoz_ArraySwapTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Emre_Elagoz_ArraySwapTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = { 1, 2, 3, 4, 5 };
+
+            int[] newAges = { 7, 8, 9, 10, 11 };
+
+            Console.WriteLine(string.Join(",", nums));
+
+            Console.WriteLine(string.Join(",", newAges));
+            Console.WriteLine(" ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                int a = nums[i];
+                int b = newAges[i];
+
+                nums[i] = b;
+                newAges[i] = a;
+            }
+
+            Console.WriteLine(string.Join(",", nums));
+            Console.WriteLine(string.Join(",", newAges));
+        }
+    }
+}
+
